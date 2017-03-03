81aae1587245ab612f8e9eab4a79a4732ec7e914
syfshen <syfshen@cn.ibm.com>
2017-03-03 15:10:28 +0800

    file to string and string to file

---
+++ workspace/src/com/ceair/wsdl/jdbc/FileUtil.java
@@ -0,0 +1,81 @@
+package com.ceair.wsdl.jdbc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class FileUtil {
+
+    private static final int DEFAULT_BUFFER_SIZE = 1024;
+
+    public static String file2String(File file, String encoding) {
+        InputStreamReader reader = null;
+        StringWriter writer = new StringWriter();
+        try {
+            if (encoding == null || "".equals(encoding.trim())) {
+                reader = new InputStreamReader(new FileInputStream(file), encoding);
+            } else {
+                reader = new InputStreamReader(new FileInputStream(file));
+            }
+
+            char[] buffer = new char[DEFAULT_BUFFER_SIZE];
+            int n = 0;
+            while (-1 != (n = reader.read(buffer))) {
+                writer.write(buffer, 0, n);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (reader != null)
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+
+        return writer.toString();
+    }
+
+    public static boolean string2File(String res, String filePath) {
+        boolean flag = true;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            File distFile = new File(filePath);
+            if (!distFile.getParentFile().exists())
+                distFile.getParentFile().mkdirs();
+            bufferedReader = new BufferedReader(new StringReader(res));
+            bufferedWriter = new BufferedWriter(new FileWriter(distFile));
+            char buf[] = new char[1024]; 
+            int len;
+            while ((len = bufferedReader.read(buf)) != -1) {
+                bufferedWriter.write(buf, 0, len);
+            }
+            bufferedWriter.flush();
+            bufferedReader.close();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            flag = false;
+            return flag;
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return flag;
+    }
+
+}
