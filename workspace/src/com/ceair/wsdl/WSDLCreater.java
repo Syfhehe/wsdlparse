97ed33a588d1abfc734550d47ce3e16f7fd82f55
syfshen <syfshen@cn.ibm.com>
2017-03-05 21:04:32 +0800

    finish wsdl creater

--- workspace/src/com/ceair/wsdl/WSDLCreater.java
+++ workspace/src/com/ceair/wsdl/WSDLCreater.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
 
-import javax.naming.Binding;
+import javax.wsdl.BindingOperation;
 import javax.wsdl.Definition;
 import javax.wsdl.Fault;
 import javax.wsdl.Message;
@@ -25,66 +23,64 @@ import com.ceair.wsdl.domain.ServiceVersion;
 import com.ceair.wsdl.jdbc.FileUtil;
 import com.ceair.wsdl.jdbc.OracleDBUtil;
 import com.ibm.wsdl.OperationImpl;
-import com.ibm.wsdl.extensions.soap.SOAPBindingImpl;
-import com.ibm.wsdl.extensions.soap12.SOAP12BindingImpl;
+import com.ibm.wsdl.extensions.soap.SOAPOperationImpl;
+import com.ibm.wsdl.extensions.soap12.SOAP12OperationImpl;
 
-import oracle.jpub.runtime.OracleDataUtil;
 
 public class WSDLCreater {
 
     public static void main(String args[]) {
         ServiceOperation serviceOperation = OracleDBUtil.selectServiceOperation(4);
-        createWSDL(serviceOperation);
+        ServiceVersion serviceVersion = OracleDBUtil.selectServiceVersion(serviceOperation.getServiceVerId());
+        createWSDL(serviceOperation, serviceVersion);
     }
 
-    public static void createWSDL(ServiceOperation serviceOperation) {
-
-        ServiceVersion serviceVersion = OracleDBUtil.selectServiceVersion(serviceOperation.getServiceVerId());
+    public static void createWSDL(ServiceOperation serviceOperation, ServiceVersion serviceVersion) {
+        //从数据库中得到ServiceOperation对应的ServiceVersion， 并将其wsdl_Clob保存成wsdl文件方便读取
         String wsdlString = serviceVersion.getWsdlClob();
         String wsdlLocation = serviceVersion.getWsdlLocation();
-        FileUtil.string2File(wsdlString, wsdlLocation);
+        String filePathTemp = FileUtil.string2File(wsdlString, wsdlLocation);
 
         try {
+            //读取刚才保存的wsdl文件 文件内容和数据库中的wsdl_clob一致
             WSDLFactory wsdlFactory = WSDLFactory.newInstance();
             WSDLReader reader = wsdlFactory.newWSDLReader();
             reader.setFeature("javax.wsdl.verbose", true);
             reader.setFeature("javax.wsdl.importDocuments", true);
-
             Definition def = reader.readWSDL(wsdlLocation);
-
+            FileUtil.deleteFile(filePathTemp);
+            //遍历整个PortTypes
             Map portTypeMap = def.getAllPortTypes();
             Iterator portTypeItr = portTypeMap.entrySet().iterator();
             while (portTypeItr.hasNext()) {
+                //新的Operation List用来替换原来的Operation
+                List<Operation> portTypeOperationListModify = new ArrayList();
                 Map.Entry portTypeEntry = (Map.Entry) portTypeItr.next();
                 PortType portType = (PortType) portTypeEntry.getValue();
                 List<Operation> portTypeOperationList = portType.getOperations();
-                // new port list
-                List<Operation> portTypeOperationListModify = new ArrayList();
                 Iterator<Operation> portTypeOperationItr = portTypeOperationList.iterator();
                 while (portTypeOperationItr.hasNext()) {
                     Operation portTypeOperation = (OperationImpl) portTypeOperationItr.next();
                     System.out.println("Operation Name:" + portTypeOperation.getName());
-                    // modify input message
+                    // 修改 input message
                     if (portTypeOperation.getInput() != null) {
                         Message messageInput = portTypeOperation.getInput().getMessage();
                         QName qNameInput = new QName(serviceOperation.getOptInputMsgNs(),
                                 serviceOperation.getOptInputMsgName());
                         messageInput.setQName(qNameInput);
                         portTypeOperation.getInput().setMessage(messageInput);
-
                         def.addNamespace("inputtns", serviceOperation.getOptInputMsgNs());
                     }
-                    // modify output message
+                    // 修改 output message
                     if (portTypeOperation.getOutput() != null) {
                         Message messageOutput = portTypeOperation.getOutput().getMessage();
                         QName qNameOutput = new QName(serviceOperation.getOptOutputMsgNs(),
                                 serviceOperation.getOptOutputMsgName());
                         messageOutput.setQName(qNameOutput);
                         portTypeOperation.getOutput().setMessage(messageOutput);
-
                         def.addNamespace("outputtns", serviceOperation.getOptOutputMsgNs());
                     }
-                    // modify fault message
+                    // 修改 fault message
                     if (portTypeOperation.getFault(serviceOperation.getOptSrcFaultMsgName()) != null) {
                         Fault fault = portTypeOperation.getFault(serviceOperation.getOptSrcFaultMsgName());
                         Message messageFault = fault.getMessage();
@@ -94,43 +90,53 @@ public class WSDLCreater {
                         fault.setMessage(messageFault);
                         portTypeOperation.removeFault(serviceOperation.getOptSrcFaultMsgName());
                         portTypeOperation.addFault(fault);
+                        def.addNamespace("faulttns", serviceOperation.getOptFaultMsgName());
+
                     }
                     portTypeOperationListModify.add(portTypeOperation);
                 }
             }
-            // replace
+            // 修改 soap action, binding--operation--bindingOperationElement(替换SoapAction)--operation（替换BindingOperation）--binding（替换binding）
             Map bindingMap = def.getAllBindings();
             Iterator bindingItr = bindingMap.entrySet().iterator();
             while (bindingItr.hasNext()) {
                 Map.Entry bindingEntry = (Map.Entry) bindingItr.next();
                 def.removeBinding((QName) bindingEntry.getKey());
                 javax.wsdl.Binding binding = (javax.wsdl.Binding) bindingEntry.getValue();
-                if (binding.getQName().getLocalPart().equals(serviceOperation.getOptSrcEnName()) ) {
-                    ExtensibilityElement bindingElement = (ExtensibilityElement) binding.getExtensibilityElements()
-                            .get(0);
-                    ExtensibilityElement newBindingElement = setTransportURI(bindingElement,
-                            serviceOperation.getOptSoapAction());
-                    binding.removeExtensibilityElement(bindingElement);
-                    binding.addExtensibilityElement(newBindingElement);
+                List bindingOperations = binding.getBindingOperations();
+                Iterator bindingOperationItr = bindingOperations.iterator();
+                while (bindingOperationItr.hasNext()) {
+                    BindingOperation bindingOperation = (BindingOperation) bindingOperationItr.next();
+                    if(bindingOperation.getName().equals(serviceOperation.getOptSrcEnName())){
+                        binding.getBindingOperations().remove(bindingOperation);
+                        ExtensibilityElement bindingOperationElement = (ExtensibilityElement) bindingOperation.getExtensibilityElements().get(0);                   
+                        ExtensibilityElement newBindingOperationElement = setSoapAction(bindingOperationElement,
+                                serviceOperation.getOptSoapAction());
+                        bindingOperation.removeExtensibilityElement(bindingOperationElement);
+                        bindingOperation.addExtensibilityElement(newBindingOperationElement); 
+                        binding.addBindingOperation(bindingOperation);
+                    }
                 }
                 def.addBinding(binding);
             }
-            
+
             WSDLWriter wirter = wsdlFactory.newWSDLWriter();
             wirter.writeWSDL(def, System.out);
         } catch (WSDLException e) {
             e.printStackTrace();
         }
-
     }
 
-    private static ExtensibilityElement setTransportURI(ExtensibilityElement bindingElement, String targetSoapAction) {
-        if (bindingElement instanceof SOAPBindingImpl) {
-            ((SOAPBindingImpl) bindingElement).setTransportURI(targetSoapAction);
-            return bindingElement;
-        } else if (bindingElement instanceof SOAP12BindingImpl) {
-            ((SOAP12BindingImpl) bindingElement).setTransportURI(targetSoapAction);
-            return bindingElement;
+    private static ExtensibilityElement setSoapAction(ExtensibilityElement extensibilityElement, String targetSoapAction) {
+
+        if (extensibilityElement instanceof SOAP12OperationImpl) {
+             ((SOAP12OperationImpl) extensibilityElement).setSoapActionURI(targetSoapAction);
+            return extensibilityElement;
+
+        } else if (extensibilityElement instanceof SOAPOperationImpl) {
+             ((SOAPOperationImpl) extensibilityElement).setSoapActionURI(targetSoapAction);
+             return extensibilityElement;
+
         }
         return null;
     }
